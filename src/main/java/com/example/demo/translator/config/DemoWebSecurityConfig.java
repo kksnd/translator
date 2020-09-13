ff3bb617281183ce2b65b57e2eab2a22330c1f0b
Thousanda <k0k1senda@gmail.com>
2020-09-13 15:05:21 +0900

    implemented translation page without csrf token

--- src/main/java/com/example/demo/translator/config/DemoWebSecurityConfig.java
+++ src/main/java/com/example/demo/translator/config/DemoWebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +24,9 @@ class DemoWebSecurityConfig extends WebSecurityConfigurerAdapter {
             // LOGIN
             .formLogin()
             /* */.defaultSuccessUrl("/success")
+            .and()
+            .csrf().disable()
+            //.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
         // end
         ;
     }
--- src/main/java/com/example/demo/translator/controller/TranslationController.java
+++ src/main/java/com/example/demo/translator/controller/TranslationController.java
@@ -7,53 +7,54 @@ import com.example.demo.translator.model.TranslatedResponseModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class TranslationController {
     @GetMapping(path = "hc", produces = "text/html")
-    @ResponseBody
-    public String healthCheck() {
+    public String healthCheck(Model model) {
         RestTemplate restTemplate = new RestTemplate();
 
         String URL = "http://localhost:5000/healthcheck/json";
         HelloResponseModel helloResponseModel =
                 restTemplate.getForObject(URL, HelloResponseModel.class);
         String response = helloResponseModel.getHello();
-        return response;
+        if (response == null) {
+            model.addAttribute("message", "null response");
+        } else {
+            model.addAttribute("message", response);
+        }
+        return "hello";
     }
 
 
     @GetMapping(path = "translation", produces = "text/html")
-    @ResponseBody
-    public String translation(Model model) {
+    public String translationGet(Model model) {
+        model.addAttribute("source", "");
+        model.addAttribute("target", "");
+        return "translation";
+    }
+
+
+    @PostMapping(path = "translation", produces = "text/html")
+    public String translation(Model model, @RequestParam("source") String source) {
         RestTemplate restTemplate = new RestTemplate();
-
-        /*
-        // JSON GET
-        String URL = "http://localhost:5000/mock/translate/json";
-        TranslatedJsonResponseModel translatedResponseModel =
-                restTemplate.getForObject(URL, TranslatedJsonResponseModel.class);
-        String translatedText = translatedResponseModel.getResponse().getText();
-        */
-
-        /*
-        // XML GET
-        String URL = "http://localhost:5000/mock/translate/xml";
-        TranslatedResponseModel translatedResponseModel =
-                restTemplate.getForObject(URL, TranslatedResponseModel.class);
-        String translatedText = translatedResponseModel.getText();
-        */
 
         // XML POST
         String URL = "http://localhost:5000/translate/to_ja";
         TranslateRequestModel request = new TranslateRequestModel();
-        request.setText("How are you?");
+        request.setText(source);
         TranslatedResponseModel translatedResponseModel = restTemplate.postForObject(URL, request, TranslatedResponseModel.class);
         String translatedText = translatedResponseModel.getText();
 
         System.out.println(translatedText);
-        return translatedText;
+
+        model.addAttribute("source", source);
+        model.addAttribute("target", translatedText);
+
+        return "translation";
     }
 }
